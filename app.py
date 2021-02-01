a0b82b189150b837888fae658f69891d084fe666
Wilfredo J. Molina <molina.wilfredo.j@gmail.com>
2021-02-01 23:21:02 +0000

    init commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import arrow
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/wjm/application/sqlite3.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(120), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+    user_agent = db.Column(db.String(120))
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST' and request.form['text']:
+        db.session.add(Comment(
+            text=request.form['text'],
+            user_agent=request.headers['User-Agent']
+        ))
+        db.session.commit()
+    comments = Comment.query.all()
+    for comment in comments:
+        comment.created_on = arrow.get(comment.created_on).humanize()
+    return render_template('index.html', comments=comments[::-1])
