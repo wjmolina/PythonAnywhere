d24dfdbe45f46c704a95cef5857238c0c094ae5e
Wil <molina.wilfredo.j@gmail.com>
2021-05-31 15:17:34 -0500

    init

---
+++ models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from app import db
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(120), nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_agent = db.Column(db.String(120), nullable=False)
+
+
+class AnonymousName(db.Model):
+    anonymous_name = db.Column(db.String(120), primary_key=True)
+    user_agent = db.Column(
+        db.String(120), db.ForeignKey("comment.user_agent"), unique=True
+    )
+
+
+class WallpaperData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String, nullable=False)
+    wallpaper = db.Column(db.String, nullable=False)
+    count = db.Column(db.Integer, nullable=False, default=0)
+    created_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class IpNotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String, nullable=False)
+    note = db.Column(db.String, nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+class UhComments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
