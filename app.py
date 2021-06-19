465c1165dd974830706155b1eee7d963fc79fcd2
Wil <molina.wilfredo.j@gmail.com>
2021-06-18 19:45:16 -0500

    more cleanup

--- app.py
+++ app.py
@@ -541,7 +541,7 @@ def gomoku_board(ip):
         total_seconds=get_move_timedelta(game).total_seconds(),
         your_elo=f"{player.elo:0.0f}",
         opponent_elo=f"{opponent.elo:0.0f}" if opponent else "???",
-        your_name=(player.name or "").strip(),
+        your_name=(player.name or "").strip() or None,
         opponent_name=(opponent.name if opponent else "").strip() or None,
         last_move=game.last_move,
         players=Player.query.filter(
